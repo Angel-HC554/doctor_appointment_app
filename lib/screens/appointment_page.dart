3975804ede1fde04f46c0ca524c049298da50a28
Angel-HC554 <angelhoil554@gmail.com>
2025-04-08 13:53:14 -0600

    Build Appointment Page

--- lib/screens/appointment_page.dart
+++ lib/screens/appointment_page.dart
@@ -7,9 +7,42 @@ class AppointmentPage extends StatefulWidget {
   State<AppointmentPage> createState() => _AppointmentPageState();
 }
 
+enum FilterStatus { upcoming, complete, cancel }
+
 class _AppointmentPageState extends State<AppointmentPage> {
+  FilterStatus status = FilterStatus.upcoming; //initial status
+  Alignment _alignment = Alignment.centerLeft;
+  List<dynamic> schedules = [
+    {
+      "doctor_name": "Richard Tan",
+      "doctor_profile": "assets/doctor_2.jpg",
+      "category": "Dental",
+      "status": "upcoming",
+    },
+    {
+      "doctor_name": "Max Lim",
+      "doctor_profile": "assets/doctor_3.jpg",
+      "category": "Cardiology",
+      "status": "complete",
+    },
+    {
+      "doctor_name": "Jane Wong",
+      "doctor_profile": "assets/doctor_4.jpg",
+      "category": "Respiration",
+      "status": "complete",
+    },
+    {
+      "doctor_name": "Jenny Song",
+      "doctor_profile": "assets/doctor_5.jpg",
+      "category": "General",
+      "status": "cancel",
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
+    //return filtered appointment
+    List<dynamic> filteredSchedules = [];
     return Center(child: Text('Appointment Page'));
   }
 }
